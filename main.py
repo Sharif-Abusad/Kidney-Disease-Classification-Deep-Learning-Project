2a7278ebab276917cfc3421fb8f4c81bc7ba8e8f
Sharif Abusad <abusharif866@gmail.com>
2026-07-06 22:08:35 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.kidneyDisease import logger
+from kidneyDisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom log")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+    
